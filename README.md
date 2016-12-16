cb5870ef5ec51dc79e502dc509670a79a5484229
Prati <prati@Sagars-Mac.local>
2016-12-16 12:09:36 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is my first portfolio webpage. - Pratiksha Nikam
+ 
---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*$("#bigHeader").typeIt({
+  strings: ["I'm Pratiksha"],
+  speed:150,
+  callback:function(){ */
+  
+  $("#smallHeader").typeIt(
+  {
+  strings: ["a Web Designer"],
+  speed:50,
+  });
+$('#smallHeader').find('.ti-cursor').addClass('is-hidden');
+/*}
+  
+}); */
+
